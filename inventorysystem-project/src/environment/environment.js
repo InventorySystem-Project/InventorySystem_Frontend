e46d45379509d6dc09b221d77944c0b796f0374c
haroldsikos123 <jhonharoldsikosastete@gmail.com>
2025-10-25 19:21:08 -0500

    Update environment.js

--- inventorysystem-project/src/environment/environment.js
+++ inventorysystem-project/src/environment/environment.js
@@ -3,5 +3,5 @@ export const environment = {
   appName: 'Sistema de Inventario',
   version: '1.0.0',
   debugMode: true,
-  url: 'https://systeminventory-backend.jhonsikos.xyz/', 
+  url: 'https://systeminventory-backend.jhonsikos.xyz', 
 };
